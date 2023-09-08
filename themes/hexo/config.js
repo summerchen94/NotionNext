47829df09c80be97221b668f8113b7613f2d68cd
summerchen94 <132306538+summerchen94@users.noreply.github.com>
2023-09-08 17:26:44 +0200

    Update config.js
    菜单修改

--- themes/hexo/config.js
+++ themes/hexo/config.js
@@ -11,9 +11,9 @@ const CONFIG = {
 
   // 菜单配置
   MENU_INDEX: true, // 显示首页
-  MENU_CATEGORY: true, // 显示分类
-  MENU_TAG: true, // 显示标签
-  MENU_ARCHIVE: true, // 显示归档
+  MENU_CATEGORY: false, // 显示分类
+  MENU_TAG: false, // 显示标签
+  MENU_ARCHIVE: false, // 显示归档
   MENU_SEARCH: true, // 显示搜索
 
   POST_LIST_COVER: true, // 列表显示文章封面

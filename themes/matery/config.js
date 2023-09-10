866d3e4a813f3a956c79fc48876bc06067c1b99b
summerchen94 <132306538+summerchen94@users.noreply.github.com>
2023-09-10 19:27:52 +0200

    Update config.js

--- themes/matery/config.js
+++ themes/matery/config.js
@@ -11,8 +11,8 @@ const CONFIG = {
 
   // 菜单配置
   MENU_CATEGORY: true, // 显示分类
-  MENU_TAG: true, // 显示标签
-  MENU_ARCHIVE: true, // 显示归档
+  MENU_TAG: false, // 显示标签
+  MENU_ARCHIVE: false, // 显示归档
   MENU_SEARCH: true, // 显示搜索
 
   POST_LIST_COVER: true, // 文章封面

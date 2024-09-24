cd736899b12d2021fa83a466eeb903e593126643
summerchen94 <132306538+summerchen94@users.noreply.github.com>
2024-09-24 10:01:02 +0200

    Update blog.config.js
    开启二级菜单

--- blog.config.js
+++ blog.config.js
@@ -15,10 +15,10 @@ const BLOG = {
   // 3.14.1版本后，欢迎语在此配置，英文逗号隔开 ,  即可支持多个欢迎语打字效果。
   GREETING_WORDS: process.env.NEXT_PUBLIC_GREETING_WORDS || 'Explore everything about SEO 🎉',
 
-  CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
+  CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || true, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Summer', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || 'Explore everything about SEO', // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO || 'SEO learning', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://notion-next-blue-omega.vercel.app', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客, blog, SEO', // 网站关键词 英文逗号隔开
 
